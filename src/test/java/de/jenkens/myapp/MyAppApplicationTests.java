8eb3ffaf8d3dfa4d965299092a9604921a2b5402
Rezan Hannan <hrezan@uni-bremen.de>
2021-01-12 21:03:30 +0100

    project generated

---
+++ src/test/java/de/jenkens/myapp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jenkens.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
