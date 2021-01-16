0e730dfc2943241cb98fb19b76e36f96dd1fa5ad
Rezan Hannan <hrezan@uni-bremen.de>
2021-01-16 22:28:57 +0100

    Update TestController.java
    github plugin installed

--- src/main/java/de/jenkens/myapp/controller/TestController.java
+++ src/main/java/de/jenkens/myapp/controller/TestController.java
@@ -16,6 +16,6 @@ public class TestController {
 
 	@GetMapping
 	public String callTest() {
-		return "Test was Called -jenkins successful - auto trigger";
+		return "Test was Called -jenkins successful - auto trigger github plugin installed on jenkins";
 	}
 }
